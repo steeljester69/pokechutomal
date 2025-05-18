e4ef1a10d000476f9679b945c865dafc96a5db3b
Leonardo Mallea <profeleopokemonsc@gmail.com>
2025-05-18 19:43:05 -0400

    lol

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const attackContainer = document.querySelector(".attack-container");
 const restartBtn = document.querySelector(".restart-game");
 let playerOneHealth = 70;
 let playerTwoHealth = 70;
-const playerTwoAttacks = ["Psychic", "Confusion", "Facade", "Psycho Blast"];
+const playerTwoAttacks = ["Hoja afilada", "Placaje", "Muere Kevin", "Justo en el kevin"];
 
-// Changes health color for player one and player two
+// Cambia de color la barra de vida 
 const healthColor = (playerHealth, playerBar) => {
   if (playerHealth < 40 && playerHealth > 20) {
     playerBar.style.backgroundColor = "#cee809";
@@ -24,9 +24,9 @@ const healthColor = (playerHealth, playerBar) => {
   }
 };
 
-// Checks if Charmander or Mewtwo win
+// Chequea quien putas gana 
 const checkWinner = (name, playerBar, playerCount) => {
-  gameStateText.innerText = `${name} wins! Press restart to play again!`;
+  gameStateText.innerText = `${name} gano! Presionar reiniciar para jugar denuevo!`;
   playerBar.style.width = "0%";
   gameStateText.style.display = "block";
   attackContainer.style.display = "none";
@@ -34,28 +34,28 @@ const checkWinner = (name, playerBar, playerCount) => {
   restartBtn.style.display = "block";
 };
 
-// Player One Attacking logic function
+// Logica de que el poke que vaya primero
 const playerOneAttack = (subtract, missCount, attackName) => {
-  // Gets random number to calculate if it's a miss
+  // Te da un numero random si fallo 
   const randomNumber = Math.floor(Math.random() * missCount) + 1;
   if (randomNumber !== 1) {
-    // Subtract health
+    // La wea que quita la vida fuck 
     playerTwoHealth -= subtract;
     if (playerTwoHealth <= 0) {
-      checkWinner("Charmander", playerTwoHealthBar, playerTwoHealthCount);
+      checkWinner("charmeleon", playerTwoHealthBar, playerTwoHealthCount);
     } else if (playerOneHealth <= 0) {
-      checkWinner("Mewtwo", playerOneHealthBar, playerOneHealthCount);
+      checkWinner("ivysaur", playerOneHealthBar, playerOneHealthCount);
     } else {
       intervalFunction();
       playerTwoHealthBar.style.width = `${playerTwoHealth}%`;
       playerTwoHealthBar.style.transition = "1.4s";
       playerTwoHealthCount.innerText = `${playerTwoHealth} / 70`;
-      gameStateText.innerText = `Charmander used ${attackName}! It took away ${subtract} HP!`;
+      gameStateText.innerText = `charmeleon uso ${attackName}! Quito ${subtract} de HP!`;
       attackContainer.style.display = "none";
     }
-    // Miss game state text
+    // Fallo el ataque
   } else {
-    gameStateText.innerText = `Charmander used ${attackName}... But it missed!`;
+    gameStateText.innerText = `charmeleon uso ${attackName}... Pero Fallo!`;
     intervalFunction();
   }
 };
@@ -65,13 +65,13 @@ const playerTwoAttack = () => {
   let subtractHealth;
   const randomAttackNum = Math.floor(Math.random() * 4) + 0;
   const randomAttack = playerTwoAttacks[randomAttackNum];
-  if (randomAttack === "Psychic") {
+  if (randomAttack === "Hoja afilada") {
     missCount = 10;
     subtractHealth = 15;
-  } else if (randomAttack === "Confusion") {
+  } else if (randomAttack === "Placaje") {
     missCount = 7;
     subtractHealth = 17;
-  } else if (randomAttack === "Facade") {
+  } else if (randomAttack === "Muere Kevin") {
     missCount = 4;
     subtractHealth = 20;
   } else {
@@ -82,23 +82,23 @@ const playerTwoAttack = () => {
   if (randomNumber !== 1) {
     playerOneHealth -= subtractHealth;
     if (playerOneHealth <= 0) {
-      checkWinner("Mewtwo", playerOneHealthBar, playerOneHealthCount);
+      checkWinner("ivysaur", playerOneHealthBar, playerOneHealthCount);
     } else {
       playerOneHealthBar.style.width = `${playerOneHealth}%`;
       playerOneHealthBar.style.transition = "1.4s";
       playerOneHealthCount.innerText = `${playerOneHealth} / 70`;
-      gameStateText.innerText = `Mewtwo used ${randomAttack}! It took away ${subtractHealth} HP!`;
+      gameStateText.innerText = `ivysaur uso ${randomAttack}! Quito ${subtractHealth} de HP!`;
       attackContainer.style.display = "none";
     }
   } else {
-    gameStateText.innerText = `Mewtwo used ${randomAttack}... But it missed!`;
+    gameStateText.innerText = `ivysaur uso ${randomAttack}... Pero Fallo!`;
   }
 };
 
 setInterval(() => {
   healthColor(playerOneHealth, playerOneHealthBar);
 }, 500);
-// Setting interval for the game state auto reading text
+// Intervalo de texto -  tambien deberia funcionar para las animaciones creo 
 const intervalFunction = () => {
   healthColor(playerTwoHealth, playerTwoHealthBar);
   const interval = setInterval(() => {
@@ -118,7 +118,7 @@ const intervalFunction = () => {
   }, 5004);
 };
 
-// Restart game logic
+// Logica de reiniciar juego 
 const restartGame = () => {
   playerTwoHealthBar.style.width = "70%";
   gameStateText.style.display = "none";
@@ -133,17 +133,17 @@ const restartGame = () => {
   playerTwoHealth = 70;
 };
 
-// Restart button event listener
+// La wea que dijo el man jsd 
 restartBtn.addEventListener("click", () => restartGame());
 
-// Player one attack button events
-attackOne.addEventListener("click", () => playerOneAttack(12, 18, "Body Slam"));
+// Eventitos 
+attackOne.addEventListener("click", () => playerOneAttack(12, 18, "Corte"));
 attackTwo.addEventListener("click", () =>
-  playerOneAttack(20, 8, "Fire Breath")
+  playerOneAttack(20, 8, "Llamarada")
 );
 attackThree.addEventListener("click", () =>
-  playerOneAttack(24, 4, "Annihilate")
+  playerOneAttack(24, 4, "Cuchillada")
 );
 attackFour.addEventListener("click", () =>
-  playerOneAttack(29, 2, "Flamethrower")
+  playerOneAttack(29, 2, "Lansallamas")
 );
